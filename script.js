4cbbe5cda62c45e5e90ddc41cf9b52f6c8574b3e
LeonardoBottonaUniTn <95630334+LeonardoBottonaUniTn@users.noreply.github.com>
2021-12-07 11:14:08 +0100

    Initial project version

---
+++ script.js
@@ -0,0 +1,7 @@
+const { application } = require("express");
+
+const logo = document.createElement('img');
+logo.setAttribute = ('class', 'foto');
+logo.src = 'Logo/Logo5.jpg';
+
+app.appendChild(logo);
