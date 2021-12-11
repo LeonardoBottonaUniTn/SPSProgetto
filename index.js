373c08b2cf36cda8a60bce79fdb193f9cf5f090a
SartoRiccardo <rikki.sarto@gmail.com>
2021-12-11 22:42:09 +0100

    Documentazione

--- index.js
+++ index.js
@@ -74,29 +74,124 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       }
     }
 
-    /* DISPOSITIVI */
-
+    /**
+     * @openapi
+     * /dispositivi:
+     *   get:
+     *     summary: Restituisce una lista di dispositivi.
+     *     responses:
+     *       '200':
+     *         description: Un Array JSON di dispositivi.
+     */
     app.get('/dispositivi', (req, res) => {
       const db = client.db(DATABASE);
       const cursor = db.collection('dispositivo').find();
       cursor.toArray().then(results => res.json(results));
     });
 
+    /**
+     * @openapi
+     * /dispositivi/{id_dispositivo}:
+     *   get:
+     *     summary: Restituisce un dispositivo.
+     *     parameters:
+     *       - name: id_dispositivo
+     *         in: path
+     *         required: true
+     *         description: L'ID del dispositivo.
+     *     responses:
+     *       '200':
+     *         description: Un dispositivo.
+     *       '404':
+     *         description: "`null`: Nessun dispositivo con quell'ID è stato trovato."
+     *       '400':
+     *         description: L'ID inserito è invalido.
+     */
     app.get('/dispositivi/:id', (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID del dispositivo invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
 
       const db = client.db(DATABASE);
       db.collection('dispositivo').findOne(id)
-        .then(results => res.json(results))
+        .then(results => res.status(results ? 200 : 404).json(results))
         .catch(err => res.send(err));
     });
 
+    /**
+     * @openapi
+     * /dispositivi/{id_dispositivo}/consumo:
+     *   get:
+     *     summary: Restituisce i kWh consumati in un periodo di tempo.
+     *     parameters:
+     *       - name: id_dispositivo
+     *         in: path
+     *         required: true
+     *         description: L'ID del dispositivo.
+     *     description: |
+     *       Restituisce i kWh consumati da un dispositivo in un periodo di tempo. Si possono anche specificare la data di inizio e di fine nel body della richiesta.
+     *       ```json
+     *       {
+     *         "dateStart": "YYYY-MM-DD",
+     *         "dateEnd": "YYYY-MM-DD"
+     *       }
+     *       ```
+     *       Entrambi gli attributi sono opzionali.
+     *     responses:
+     *       '200':
+     *         description: Il consumo in kWh del dispositivo.
+     *       '404':
+     *         description: "Nessun dispositivo con quell'ID è stato trovato."
+     *       '400':
+     *         description: L'ID inserito è invalido.
+     */
+    app.get("/dispositivi/:id/consumo", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.status(400).json({ errore: "ID della proprietà invalido."});
+        return;
+      }
+      let { datetimeStart, datetimeEnd } = req.body;
+      console.log(datetimeEnd,datetimeStart);
+
+      const db = client.db(DATABASE);
+      let sum = db.collection('consumi').find({
+        "DispositivoId": id, "Date": { $gte: datetimeStart, $lt: datetimeEnd }
+      })
+        .toArray()
+        .then(calculateResults(res))
+        .catch(err => res.send(err));
+    });
+
+    /**
+     * @openapi
+     * /dispositivi:
+     *   post:
+     *     summary: Modifica un dispositivo.
+     *     description: |
+     *       Aggiunge un dispositivo con i dati passati. Il body della richiesta deve contenere un JSON come segue:
+     *       ```json
+     *       {
+     *         ConsumiDichiarati: "float"
+     *         DispositivoName: "string",
+     *         Locazione: {
+     *           tipo: "stanza|proprietà",
+     *           id: "string",
+     *         }
+     *       }
+     *       ```
+     *     responses:
+     *       '201':
+     *         description: Il dispositivo è stato aggiunto.
+     *       '400':
+     *         description: I dati inseriti non sono corretti.
+     */
     app.post('/dispositivi', (req, res) => {
       const db = client.db(DATABASE);
       const collection = db.collection('dispositivo');
@@ -107,22 +202,68 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         .catch(error => console.error(error));
     });
 
+    /**
+     * @openapi
+     * /dispositivi/{id_dispositivo}:
+     *   put:
+     *     summary: Modifica un dispositivo.
+     *     parameters:
+     *       - name: id_dispositivo
+     *         in: path
+     *         required: true
+     *         description: L'ID del dispositivo.
+     *     description: |
+     *       Modifica i dati passati. Il body della richiesta deve contenere un JSON come segue:
+     *       ```json
+     *       {
+     *         ConsumiDichiarati: "float"
+     *         DispositivoName: "string",
+     *         Locazione: {
+     *           tipo: "stanza|proprietà",
+     *           id: "string",
+     *         }
+     *       }
+     *       ```
+     *     responses:
+     *       '200':
+     *         description: Il dispositivo è stato modificato.
+     *       '400':
+     *         description: L'ID inserito è invalido o non sono corretti i dati.
+     *       '404':
+     *         description: Il dispositivo non esiste.
+     */
     app.put("/dispositivi/:id", (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID del dispositivo invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
     });
 
+    /**
+     * @openapi
+     * /dispositivi/{id_dispositivo}:
+     *   delete:
+     *     summary: Cancella un dispositivo.
+     *     parameters:
+     *       - name: id_dispositivo
+     *         in: path
+     *         required: true
+     *         description: L'ID del dispositivo.
+     *     responses:
+     *       '200':
+     *         description: Il dispositivo richiesto non esiste più.
+     *       '400':
+     *         description: L'ID inserito è invalido.
+     */
     app.delete("/dispositivi/:id", (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID del dispositivo invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
 
@@ -132,14 +273,12 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         .catch(err => res.send(err));
     });
 
-    /* CONSUMI */
-
-    app.get("/consumi/proprieta/:id", (req, res) => {
+    app.get("/proprieta/:id/consumo", (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID della proprietà invalido."});
+        res.status(400).json({ errore: "ID della proprietà invalido."});
         return;
       }
       let { datetimeStart, datetimeEnd } = req.body;
@@ -177,12 +316,12 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         .catch(err => res.send(err));
     });
 
-    app.get("/consumi/stanze/:id", (req, res) => {
+    app.get("/stanze/:id/consumo", (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID della proprietà invalido."});
+        res.status(400).json({ errore: "ID della proprietà invalido."});
         return;
       }
       let { datetimeStart, datetimeEnd } = req.body;
@@ -206,27 +345,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         .catch(err => res.send(err));
     });
 
-    app.get("/consumi/dispositivi/:id", (req, res) => {
-      let id;
-      try {
-        id = MongoDB.ObjectId(req.params.id);
-      } catch(exc) {
-        res.json({ errore: "ID della proprietà invalido."});
-        return;
-      }
-      let { datetimeStart, datetimeEnd } = req.body;
-      console.log(datetimeEnd,datetimeStart);
-
-      const db = client.db(DATABASE);
-      let sum = db.collection('consumi').find({
-        "DispositivoId": id, "Date": { $gte: datetimeStart, $lt: datetimeEnd }
-      })
-        .toArray()
-        .then(calculateResults(res))
-        .catch(err => res.send(err));
-    });
-
-    app.get("/consumi/utenti/:id", (req, res) => {
+    app.get("/utenti/:id/consumo", (req, res) => {
     });
 
     /* TRIGGER */
@@ -242,7 +361,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       try {
         id = MongoDB.ObjectId(id);
       } catch(exc) {
-        res.json({ errore: "ID del trigger invalido." });
+        res.status(400).json({ errore: "ID del trigger invalido." });
         return;
       }
 
@@ -267,7 +386,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID del dispositivo invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
     });
@@ -277,7 +396,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.json({ errore: "ID del dispositivo invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
 
