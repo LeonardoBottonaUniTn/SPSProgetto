63c65815e5eae890d6c321d4851b22d314dae193
SartoRiccardo <rikki.sarto@gmail.com>
2021-12-11 20:05:54 +0100

    API consumo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var MongoClient = require ("mongodb").MongoClient;
 var MongoDB = require("mongodb");
 
 // definition of the connection string
-var CONNECTION_STRING = "mongodb+srv://G38:pass_G38@sps.94fac.mongodb.net/test";
+var CONNECTION_STRING = "mongodb+srv://G38:pass_G38@sps.94fac.mongodb.net/test";  // molto sicuro
 
 var app = Express ();
 app.use(bodyParser.json ());
@@ -64,21 +64,17 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       return;
     }
 
-    const getUser = req => {
-
-    };
+    const calculateResults = response => {
+      return results => {
+        let consumo = 0;
+        for(r of results) {
+          consumo += r.ConsumoGiornaliero;
+        }
+        response.json({ consumo });
+      }
+    }
 
     /* DISPOSITIVI */
-
-    app.post('/dispositivi', (req, res) => {
-      const db = client.db(DATABASE);
-      const collection = db.collection('dispositivo');
-      collection.insertOne(req.body)
-        .then(result => {
-          res.send(result)
-        })
-        .catch(error => console.error(error));
-    });
 
     app.get('/dispositivi', (req, res) => {
       const db = client.db(DATABASE);
@@ -101,6 +97,26 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         .catch(err => res.send(err));
     });
 
+    app.post('/dispositivi', (req, res) => {
+      const db = client.db(DATABASE);
+      const collection = db.collection('dispositivo');
+      collection.insertOne(req.body)
+        .then(result => {
+          res.send(result)
+        })
+        .catch(error => console.error(error));
+    });
+
+    app.put("/dispositivi/:id", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.json({ errore: "ID del dispositivo invalido."});
+        return;
+      }
+    });
+
     app.delete("/dispositivi/:id", (req, res) => {
       let id;
       try {
@@ -119,11 +135,97 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
     /* CONSUMI */
 
     app.get("/consumi/proprieta/:id", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.json({ errore: "ID della proprietà invalido."});
+        return;
+      }
+      let { datetimeStart, datetimeEnd } = req.body;
+
+      const db = client.db(DATABASE);
+      db.collection('stanza').find({  // Cerca le stanze dentro la proprietà
+        "Proprieta": id,
+      })
+        .toArray()
+        .then(results => {  // Cerca i dispositivi contenuti nelle stanze dentro la proprietà o nella proprietà stessa
+          return db.collection("dispositivo").find({
+            $or: [
+              {
+                "Locazione.tipo": "stanza",
+                "Locazione.id": { $in: results.map(stanza => MongoDB.ObjectId(stanza._id)) }
+              },
+              {
+                "Locazione.tipo": "proprietà",
+                "Locazione.id": id
+              }
+            ]
+          })
+          .toArray();
+        })
+        .then(results => {  // Cerca i consumi di quei dispositivi
+          return db.collection("consumi").find({
+            "DispositivoId": {
+              $in: results.map(dispositivo => MongoDB.ObjectId(dispositivo._id)),
+            },
+            "Date": { $gte: datetimeStart, $lt: datetimeEnd },
+          })
+          .toArray();
+        })
+        .then(calculateResults(res))
+        .catch(err => res.send(err));
     });
+
     app.get("/consumi/stanze/:id", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.json({ errore: "ID della proprietà invalido."});
+        return;
+      }
+      let { datetimeStart, datetimeEnd } = req.body;
+
+      const db = client.db(DATABASE);
+      db.collection('dispositivo').find({
+        "Locazione.tipo": "stanza",
+        "Locazione.id": id,
+      })
+        .toArray()
+        .then(results => {
+          return db.collection("consumi").find({
+            "DispositivoId": {
+              $in: results.map(dispositivo => MongoDB.ObjectId(dispositivo._id)),
+            },
+            "Date": { $gte: datetimeStart, $lt: datetimeEnd },
+          })
+          .toArray();
+        })
+        .then(calculateResults(res))
+        .catch(err => res.send(err));
     });
+
     app.get("/consumi/dispositivi/:id", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.json({ errore: "ID della proprietà invalido."});
+        return;
+      }
+      let { datetimeStart, datetimeEnd } = req.body;
+      console.log(datetimeEnd,datetimeStart);
+
+      const db = client.db(DATABASE);
+      let sum = db.collection('consumi').find({
+        "DispositivoId": id, "Date": { $gte: datetimeStart, $lt: datetimeEnd }
+      })
+        .toArray()
+        .then(calculateResults(res))
+        .catch(err => res.send(err));
     });
+
     app.get("/consumi/utenti/:id", (req, res) => {
     });
 
@@ -158,6 +260,16 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
           res.send(result)
         })
         .catch(error => console.error(error));
+    });
+
+    app.put("/trigger/:id", (req, res) => {
+      let id;
+      try {
+        id = MongoDB.ObjectId(req.params.id);
+      } catch(exc) {
+        res.json({ errore: "ID del dispositivo invalido."});
+        return;
+      }
     });
 
     app.delete("/trigger/:id", (req, res) => {
