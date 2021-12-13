7c6433b1726ec4550b92f18a5542d98d6802b54b
LeonardoBottonaUniTn <95630334+LeonardoBottonaUniTn@users.noreply.github.com>
2021-12-13 16:29:28 +0100

    Bottoni add HTML

--- triggers.js
+++ triggers.js
@@ -1,5 +1,7 @@
 
-console.log("ciao");
+
+
+//eliminaTrigger()
 
 // Azioni dei bottoni trigger
 var btnID = document.getElementById('eliminaTriggerButton');
@@ -8,18 +10,14 @@ console.log(btnID);
 console.log(btnModalID);
 let card;
 
-
-
 btnID.addEventListener('click', (event) => {
     console.log(event.target.id);
     if(event.target.id == btnID.id) {
         const button = event.target;
-        card = button.parentNode.parentNode.parentNode;
+        card = button.parentNode.parentNode.parentNode.parentNode;
     }
 });
 console.log(card);
-
-
 
 btnModalID.addEventListener('click', (event) => {
     console.log(event.target.id);
@@ -28,4 +26,40 @@ btnModalID.addEventListener('click', (event) => {
         }
 });
 
-//eliminaTrigger()
+
+
+//creaTrigger
+addBtn = document.getElementById('saveNewtrigger');
+let nomeDispositivo;
+let livelloSoglia;
+let iterator = 1;
+
+
+
+addBtn.addEventListener('click', addNewTrigger);
+
+function addNewTrigger(){
+    
+    nomeDispositivo = document.getElementById("nuovoTriggerDispositivo").value;
+    livelloSoglia = document.getElementById("nuovoTriggerSoglia").value;
+    
+    let newTriggerCard = '<div class="col">' +
+                    '<div class="card  mb-5 bg-body rounded" style="width:18rem">' +
+                        '<div class="card-body">' +
+                            '<h5 class="card-title"> Trigger ' + iterator++ + '</h5>' +
+                            '<p class="card-text">' +
+                                'Nome dispositivo : '+ nomeDispositivo + '<br/>' +
+                                'Livello soglia :' + livelloSoglia +
+                            '</p>' +
+                            '<div class="container">' +
+                                '<a href="#" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTrigger">Modifica</a>' +
+                                '<a href="#eliminaTrigger" id="eliminaTriggerButton" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
+    
+    document.getElementById("rowContainer").innerHTML += newTriggerCard;
+    //var n = document.createTextNode(newTriggerCard);
+    //document.getElementById("rowContainer").appendChild(n);
+}
