406f7a421bd8686b66033b2c40084c32db49f4de
SartoRiccardo <rikki.sarto@gmail.com>
2021-12-16 12:45:47 +0100

    Sanitized inputs

--- index.js
+++ index.js
@@ -61,6 +61,66 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
       }
     }
 
+    const validateBody = async (body, api) => {
+      const db = client.db(DATABASE);
+      if(api === "trigger") {
+        const allowedKeys = ["Dispositivo", "Soglia", "NomeTrigger"];
+        for(const key of Object.keys(body)) {
+          if(!allowedKeys.includes(key)) return `Parametro sconosciuto: ${key}`;
+        }
+        for(const key of allowedKeys) {
+          if(!Object.keys(body).includes(key)) return `Parametro mancante: ${key}`;
+        }
+
+        if(typeof(body.Dispositivo) !== "string") return `Dispositivo non contiene un ID valido`;
+        else {
+          try {
+            let disp = await db.collection("dispositivo").findOne(MongoDB.ObjectId(body.Dispositivo));
+            if(!disp) return `Dispositivo contiene un ID di un dispositivo inesistente`;
+          }
+          catch(exc) { return `Dispositivo non contiene un ID valido` }
+        }
+        if(typeof(body.Soglia) !== "number") return `Soglia deve essere un numero`;
+        if(typeof(body.NomeTrigger) !== "string") return `NomeTrigger deve essere una stringa`;
+      }
+      else if(api === "dispositivi") {
+        const allowedKeys = ["ConsumiDichiarati", "DispositivoName", "Locazione"];
+        for(const key of Object.keys(body))
+          if(!allowedKeys.includes(key)) return `Parametro sconosciuto: ${key}`;
+        for(const key of allowedKeys)
+          if(!Object.keys(body).includes(key)) return `Parametro mancante: ${key}`;
+
+        const allowedKeysLocazione = ["tipo", "id"];
+        for(const key of Object.keys(body.Locazione))
+          if(!allowedKeysLocazione.includes(key)) return `Parametro sconosciuto: Locazione.${key}`;
+        for(const key of allowedKeysLocazione)
+          if(!Object.keys(body.Locazione).includes(key)) return `Parametro mancante: ${key}`;
+
+        if(typeof(body.Locazione.tipo) !== "string" || !["stanza", "proprietà"].includes(body.Locazione.tipo))
+          return `Locazione.tipo deve essere "stanza" o "proprietà"`;
+        if(typeof(body.Locazione.id) !== "string") return `Locazione.id non contiene un ID valido`;
+        else {
+          try {
+            let locazione = await db.collection(body.Locazione.tipo).findOne(MongoDB.ObjectId(body.Locazione.id));
+            if(!locazione) return `Locazione.id contiene un ID di una ${body.Locazione.tipo} inesistente`;
+          }
+          catch(exc) { return `Locazione.id non contiene un ID valido` }
+        }
+        if(typeof(body.ConsumiDichiarati) !== "number") return `ConsumiDichiarati deve essere un numero`;
+        if(typeof(body.DispositivoName) !== "string") return `DispositivoName deve essere una stringa`;
+      }
+
+      return null;
+    }
+
+    const exists = async (coll, id) => {
+      try {
+        const db = client.db(DATABASE);
+        const item = await db.collection(coll).findOne(MongoDB.ObjectId(id));
+        return item !== null;
+      } catch(exc) { return false; }
+    }
+
     /**
      * @openapi
      * /dispositivi:
@@ -70,7 +130,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '200':
      *         description: Un Array JSON di dispositivi.
      */
-    app.get('/dispositivi', (req, res) => {
+    app.get('/dispositivi', async (req, res) => {
       const db = client.db(DATABASE);
       const cursor = db.collection('dispositivo').find();
       cursor.toArray().then(results => res.json(results));
@@ -94,7 +154,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.get('/dispositivi/:id', (req, res) => {
+    app.get('/dispositivi/:id', async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
@@ -127,7 +187,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *         "dateEnd": "YYYY-MM-DD"
      *       }
      *       ```
-     *       Entrambi gli attributi sono opzionali.
+     *       Entrambi gli attributi sono opzionali, e vengono ignorati se i dati inseriti non sono sintatticamente corretti.
      *     responses:
      *       '200':
      *         description: Il consumo in kWh del dispositivo.
@@ -136,16 +196,19 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.get("/dispositivi/:id/consumo", (req, res) => {
+    app.get("/dispositivi/:id/consumo", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.status(400).json({ errore: "ID della proprietà invalido."});
+        res.status(400).json({ errore: "ID del dispositivo invalido."});
+        return;
+      }
+      if(!(await exists("dispositivo", id))) {
+        res.status(404).json({ errore: "Il dispositivo non esiste." });
         return;
       }
       let { datetimeStart, datetimeEnd } = req.body;
-      console.log(datetimeEnd,datetimeStart);
 
       const db = client.db(DATABASE);
       let sum = db.collection('consumi').find({
@@ -179,14 +242,26 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: I dati inseriti non sono corretti.
      */
-    app.post('/dispositivi', (req, res) => {
+    app.post('/dispositivi', async (req, res) => {
       const db = client.db(DATABASE);
       const collection = db.collection('dispositivo');
-      collection.insertOne(req.body)
-        .then(result => {
-          res.send(result)
-        })
-        .catch(error => console.error(error));
+
+      let error = await validateBody(req.body, "dispositivi");
+      if(error) {
+        res.status(400).json({ error });
+        return;
+      }
+      const dispositivo = {
+        ...req.body,
+        Locazione: {
+          ...req.body.Locazione,
+          id: MongoDB.ObjectId(req.body.Locazione.id),
+        },
+      };
+
+      collection.insertOne(dispositivo)
+        .then(result => res.send(result))
+        .catch(error => res.status(500).send(error));
     });
 
     /**
@@ -219,7 +294,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '404':
      *         description: Il dispositivo non esiste.
      */
-    app.put("/dispositivi/:id", (req, res) => {
+    app.put("/dispositivi/:id", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
@@ -227,6 +302,23 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
+      if(!(await exists("dispositivo", id))) {
+        res.status(404).json({ errore: "La proprietà non esiste." });
+        return;
+      }
+
+      let error = await validateBody(req.body, "dispositivi");
+      if(error) {
+        res.status(400).json({ error });
+        return;
+      }
+      const dispositivo = {
+        ...req.body,
+        Locazione: {
+          ...req.body.Locazione,
+          id: MongoDB.ObjectId(req.body.Locazione.id),
+        },
+      };
 
       const db = client.db(DATABASE);
       db.collection('dispositivo').updateOne({ _id: id }, { $set: req.body })
@@ -252,7 +344,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.delete("/dispositivi/:id", (req, res) => {
+    app.delete("/dispositivi/:id", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
@@ -285,7 +377,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *         "dateEnd": "YYYY-MM-DD"
      *       }
      *       ```
-     *       Entrambi gli attributi sono opzionali.
+     *       Entrambi gli attributi sono opzionali, e vengono ignorati se i dati inseriti non sono sintatticamente corretti.
      *     responses:
      *       '200':
      *         description: Il consumo in kWh dei dispositivi nella proprietà.
@@ -294,7 +386,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.get("/proprieta/:id/consumo", (req, res) => {
+    app.get("/proprieta/:id/consumo", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
@@ -302,6 +394,11 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         res.status(400).json({ errore: "ID della proprietà invalido."});
         return;
       }
+      if(!(await exists("proprietà", id))) {
+        res.status(404).json({ errore: "La proprietà non esiste." });
+        return;
+      }
+
       let { datetimeStart, datetimeEnd } = req.body;
 
       const db = client.db(DATABASE);
@@ -355,39 +452,42 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *         "dateEnd": "YYYY-MM-DD"
      *       }
      *       ```
-     *       Entrambi gli attributi sono opzionali.
+     *       Entrambi gli attributi sono opzionali, e vengono ignorati se i dati inseriti non sono sintatticamente corretti.
      *     responses:
      *       '200':
      *         description: Il consumo in kWh dei dispositivi nella stanza.
      *       '404':
-     *         description: "Nessuna stanza con quell'ID è stato trovata."
+     *         description: Nessuna stanza con quell'ID è stata trovata.
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.get("/stanze/:id/consumo", (req, res) => {
+    app.get("/stanze/:id/consumo", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
-        res.status(400).json({ errore: "ID della proprietà invalido."});
+        res.status(400).json({ errore: "ID della stanza invalido."});
         return;
       }
+      if(!(await exists("stanza", id))) {
+        res.status(404).json({ errore: "La stanza non esiste." });
+        return;
+      }
+
       let { datetimeStart, datetimeEnd } = req.body;
 
       const db = client.db(DATABASE);
       db.collection('dispositivo').find({
         "Locazione.tipo": "stanza",
         "Locazione.id": id,
-      })
-        .toArray()
+      }).toArray()
         .then(results => {
           return db.collection("consumi").find({
             "DispositivoId": {
               $in: results.map(dispositivo => MongoDB.ObjectId(dispositivo._id)),
             },
             "Date": { $gte: datetimeStart, $lt: datetimeEnd },
-          })
-          .toArray();
+          }).toArray();
         })
         .then(calculateResults(res))
         .catch(err => res.send(err));
@@ -411,16 +511,14 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *         "dateEnd": "YYYY-MM-DD"
      *       }
      *       ```
-     *       Entrambi gli attributi sono opzionali.
+     *       Entrambi gli attributi sono opzionali, e vengono ignorati se i dati inseriti non sono sintatticamente corretti.
      *     responses:
      *       '200':
      *         description: Il consumo in kWh dei dispositivi dell'utente.
-     *       '404':
-     *         description: "Nessun utente con quell'email è stato trovato."
      *       '400':
      *         description: L'email inserita è invalida.
      */
-    app.get("/utenti/:email/consumo", (req, res) => {
+    app.get("/utenti/:email/consumo", async (req, res) => {
       const { email } = req.params;
       const { datetimeStart, datetimeEnd } = req.body;
 
@@ -470,9 +568,23 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '200':
      *         description: Un Array JSON di trigger.
      */
-    app.get("/trigger", (req, res) => {
+    app.get("/trigger", async (req, res) => {
       const db = client.db(DATABASE);
-      const cursor = db.collection('trigger').find();
+      const cursor = db.collection('trigger').aggregate([
+        {
+          $lookup: {
+            from: "dispositivo",
+            localField: "Dispositivo",
+            foreignField: "_id",
+            as: "Dispositivo",
+          }
+        },
+        {
+          $project: {
+            Dispositivo: { $arrayElemAt: [ "$Dispositivo", 0 ] }
+          }
+        }
+      ]);
       cursor.toArray().then(results => res.json(results));
     });
 
@@ -494,19 +606,37 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.get("/trigger/:id", (req, res) => {
+    app.get("/trigger/:id", async (req, res) => {
       let id;
       try {
-        id = MongoDB.ObjectId(id);
+        id = MongoDB.ObjectId(req.params.id);
       } catch(exc) {
         res.status(400).json({ errore: "ID del trigger invalido." });
         return;
       }
 
       const db = client.db(DATABASE);
-      cursor = db.collection('trigger').findOne(id)
-        .then(result => res.json(result))
-        .catch(err => res.json(err));
+      const cursor = db.collection('trigger').aggregate([
+        { $match: { _id: id } },
+        { $limit: 1 },
+        {
+          $lookup: {
+            from: "dispositivo",
+            localField: "Dispositivo",
+            foreignField: "_id",
+            as: "Dispositivo",
+          }
+        },
+        {
+          $project: {
+            Dispositivo: { $arrayElemAt: [ "$Dispositivo", 0 ] }
+          }
+        }
+      ]);
+
+      cursor.next()
+        .then(result => res.status(result ? 200 : 404).json(result))
+        .catch(err => res.status(500).json(err));
     });
 
     /**
@@ -529,14 +659,25 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: I dati inseriti non sono corretti.
      */
-    app.post("/trigger", (req, res) => {
+    app.post("/trigger", async (req, res) => {
       const db = client.db(DATABASE);
       const collection = db.collection('trigger');
-      collection.insertOne(req.body)
+
+      let error = await validateBody(req.body, "trigger");
+      if(error) {
+        res.status(400).json({ error });
+        return;
+      }
+      const trigger = {
+        ...req.body,
+        Dispositivo: MongoDB.ObjectId(req.body.Dispositivo),
+      };
+
+      collection.insertOne(trigger)
         .then(result => {
-          res.send(result)
+          res.status(201).send(result)
         })
-        .catch(error => console.error(error));
+        .catch(error => res.status(500).send({ error }));
     });
 
     /**
@@ -566,7 +707,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '404':
      *         description: Il trigger non esiste.
      */
-    app.put("/trigger/:id", (req, res) => {
+    app.put("/trigger/:id", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
@@ -574,9 +715,23 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
         res.status(400).json({ errore: "ID del dispositivo invalido."});
         return;
       }
+      if(!(await exists("trigger", id))) {
+        res.status(404).json({ errore: "Il trigger non esiste." });
+        return;
+      }
+
+      let error = await validateBody(req.body, "trigger");
+      if(error) {
+        res.status(400).json({ error });
+        return;
+      }
+      const trigger = {
+        ...req.body,
+        Dispositivo: MongoDB.ObjectId(req.body.Dispositivo),
+      };
 
       const db = client.db(DATABASE);
-      db.collection('trigger').updateOne({ _id: id }, { $set: req.body })
+      db.collection('trigger').updateOne({ _id: id }, { $set: trigger })
         .then(result => {
           res.send(result)
         })
@@ -599,7 +754,7 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
      *       '400':
      *         description: L'ID inserito è invalido.
      */
-    app.delete("/trigger/:id", (req, res) => {
+    app.delete("/trigger/:id", async (req, res) => {
       let id;
       try {
         id = MongoDB.ObjectId(req.params.id);
