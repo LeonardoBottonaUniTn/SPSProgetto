fcae53f5dcfd508556754a88de49b570927e22b6
LeonardoBottonaUniTn <95630334+LeonardoBottonaUniTn@users.noreply.github.com>
2021-12-16 09:16:53 +0100

    Funzionu Triggers.js
    Fatte le funzioni modifica e add delle cards

--- triggers.js
+++ triggers.js
@@ -1,10 +1,10 @@
-let culetto = 1;
-let cristo = 1;
+
+let iterator = 0;
 
 
 //Prende e stampa i trigger già esistenti
 
-let triggers = [
+ var triggers = [
   {
     _id: "61b1e65c3d370fa548dec3a2",
     Dispositivo: {
@@ -30,32 +30,34 @@ let triggers = [
       }
     },
     Soglia: 130
-  }
+  },
 ];
 
 
 function addTriggersCard(){  
-  console.log("ciao");
-  let numeroTriggers = triggers.length();
+
+  let numeroTriggers = triggers.length;
   
-  while(cristo <= numeroTriggers){
-    let ExistingTriggerCard = '<div class="col">' + triggers[cristo].id +
+  
+  while(iterator <= numeroTriggers){
+    let ExistingTriggerCard = '<div class="col" id="' + triggers[iterator]._id + '">' +
                       '<div class="card mb-5 bg-body rounded" style="width:18rem">' +
                           '<div class="card-body">' +
-                              '<h5 class="card-title"> Trigger ' + cristo++ + '</h5>' +
+                              '<h5 class="card-title"> Trigger ' + triggers[iterator].Dispositivo.DispositivoName + '</h5>' +
                               '<p class="card-text">' +
-                                  'Nome dispositivo : '+ triggers[cristo].DispositivoName + '<br/>' +
-                                  'Livello soglia :' + triggers[cristo].Soglia +
+                                  'Nome dispositivo : '+ triggers[iterator].Dispositivo.DispositivoName + '<br/>' +
+                                  'Livello soglia : ' + triggers[iterator].Soglia + ' kW/h' +
                               '</p>' +
                               '<div class="container">' +
-                                  '<a href="#" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTrigger">Modifica</a>' +
-                                  '<a href="#eliminaTrigger" onclick="eliminaCard(' + triggers[cristo].id + 'class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
+                                  '<a href="#" onclick="modificaCard(\'' + triggers[iterator]._id + '\')" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTriggerModal">Modifica</a>' +
+                                  '<a href="#eliminaTrigger" onclick="eliminaCard(\'' + triggers[iterator]._id + '\')" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
                               '</div>' +
                           '</div>' +
                       '</div>' +
                   '</div>';
 
-      document.getElementById("rowContainer").innerHTML += ExistingTriggerCard;
+                  document.getElementById("rowContainer").innerHTML += ExistingTriggerCard;
+                  iterator++;
   }
 };
 
@@ -64,10 +66,7 @@ function addTriggersCard(){
 //eliminaTrigger()
 
 // Azioni dei bottoni trigger
-var btnID = document.getElementById('eliminaTriggerButton');
 var btnModalID = document.getElementById('eliminaTrigger');
-console.log(btnID);
-console.log(btnModalID);
 let card;
 
 // btnID.addEventListener('click', (event) => {
@@ -78,10 +77,16 @@ let card;
 //     }
 // });
 
+function eliminaCard(id) {
+  card = document.getElementById(id);
+}
+
 btnModalID.addEventListener('click', (event) => {
     // console.log(event.target.id);
           if(event.target.id == btnModalID.id) {
+            
             card.remove();
+            
           }
 });
 
@@ -92,44 +97,89 @@ addBtn = document.getElementById('saveNewtrigger');
 let nomeDispositivo;
 let livelloSoglia;
 
-
-
-
-
 addBtn.addEventListener('click', addNewTrigger);
 
-function eliminaCard(id) {
-  card = document.getElementById(id);
-}
-
 function addNewTrigger(){
-
-    nomeDispositivo = document.getElementById("nuovoTriggerDispositivo").value;
-    livelloSoglia = document.getElementById("nuovoTriggerSoglia").value;
-
-    let newTriggerCard = '<div class="col">' +
-                    '<div class="card mb-5 bg-body rounded" style="width:18rem">' +
-                        '<div class="card-body">' +
-                            '<h5 class="card-title"> Trigger ' + cristo++ + '</h5>' +
-                            '<p class="card-text">' +
+  iterator = 0;
+  nomeDispositivo = document.getElementById("nuovoTriggerDispositivo").value;
+  livelloSoglia = document.getElementById("nuovoTriggerSoglia").value;
+  
+  let newTriggerCard = '<div class="col"> id="' +  /*idGeneratoInQualcheMdodo*/ +
+                          '<div class="card mb-5 bg-body rounded" style="width:18rem">' +
+                              '<div class="card-body">' +
+                                '<h5 class="card-title"> Trigger ' + nomeDispositivo + '</h5>' +
+                                '<p class="card-text">' +
                                 'Nome dispositivo : '+ nomeDispositivo + '<br/>' +
-                                'Livello soglia :' + livelloSoglia +
-                            '</p>' +
-                            '<div class="container">' +
-                                '<a href="#" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTrigger">Modifica</a>' +
-                                '<a href="#eliminaTrigger" id="eliminaTriggerButton" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
+                                'Livello soglia : ' + livelloSoglia +
+                                '</p>' +
+                              '<div class="container">' +
+                              '<a href="#" onclick="modificaCard(\'' + /*idGeneratoInQualcheModo*/ + '\')" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTriggerModal">Modifica</a>' +
+                                  '<a href="#eliminaTrigger" onclick="eliminaCard(\'' /*idGeneratoInQualcheModo*/ + '\')" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
                             '</div>' +
+                          '</div>' +
                         '</div>' +
-                    '</div>' +
-                '</div>';
-
+                      '</div>';
+    iterator++;
+    console.log(newTriggerCard);
     document.getElementById("rowContainer").innerHTML += newTriggerCard;
     
-    //var n = document.createTextNode(newTriggerCard);
-    //document.getElementById("rowContainer").appendChild(n);
+  }
+
+
+
+//modificaTrigger
+modifyBtn = document.getElementById('modificaTrigger');
+modifyBtn.addEventListener('click', modifyTrigger);
+let i;
+let cardId;
+
+function modificaCard(id) {
+  cardId = id;
+  i = 0;
+  console.log(cardId);
+  while(cardId != triggers[i]._id){
+    i++;
+  }
+  console.log(i);
 }
 
 
+function reloadTriggersCard(){
+  document.getElementById("rowContainer").innerHTML = '';
+  iterator = 0;
+  let numeroTriggers = triggers.length;
+  while(iterator <= numeroTriggers){  
+    let ExistingTriggerCard = '<div class="col" id="' + triggers[iterator]._id + '">' +
+                                    '<div class="card mb-5 bg-body rounded" style="width:18rem">' +
+                                      '<div class="card-body">' +
+                                        '<h5 class="card-title"> Trigger ' + triggers[iterator].Dispositivo.DispositivoName + '</h5>' +
+                                        '<p class="card-text">' +
+                                          'Nome dispositivo : '+ triggers[iterator].Dispositivo.DispositivoName + '<br/>' +
+                                          'Livello soglia : ' + triggers[iterator].Soglia + ' kW/h' +
+                                        '</p>' +
+                                        '<div class="container">' +
+                                          '<a href="#" onclick="modificaCard(\'' + triggers[iterator]._id + '\')" class="btn btn-outline-secondary btn-sm" style="margin-right: 80px;" data-bs-toggle="modal" data-bs-target="#modificaTriggerModal">Modifica</a>' +
+                                          '<a href="#eliminaTrigger" onclick="eliminaCard(\'' + triggers[iterator]._id + '\')" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaTrigger"><i class="fas fa-trash-alt" style="color: black; width: 18px;"></i></a>' +
+                                        '</div>' +
+                                      '</div>' +
+                                  '</div>' +
+                                '</div>';
+    
+    document.getElementById("rowContainer").innerHTML += ExistingTriggerCard;
+    iterator++;
+  }
+};
+
+function modifyTrigger(){
+  
+  console.log(document.getElementById("modificaNomeDispositivo").value);
+  console.log(document.getElementById("modificaLivelloSoglia").value);
+  let modificaNomeDispositivo = document.getElementById("modificaNomeDispositivo").value;
+  let modificaSoglia = document.getElementById("modificaLivelloSoglia").value;
+  triggers[i].Dispositivo.DispositivoName = modificaNomeDispositivo;
+  triggers[i].Soglia =  modificaSoglia;
+  reloadTriggersCard()
+}
 
 
 
