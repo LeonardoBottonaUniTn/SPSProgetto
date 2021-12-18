5a9120194ed3fafb2b0b6cd20d83575d3376d450
GaiaPizzutiUniTn <gaia.pizzuti@studenti.unitn.it>
2021-12-18 10:19:36 +0100

    SPS_db
    Modificato var DATABASE

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json ());
 app.use(bodyParser.urlencoded ({ extended: true }) );
 
 //name of the created MongoDB
-var DATABASE = "testDB";
+var DATABASE = "SPS_db";
 var database;
 
 const swaggerJsDoc = require ('swagger-jsdoc');
