0bb01b0d43a1df8013462e8521e7198a5fa536f3
LeonardoBottonaUniTn <95630334+LeonardoBottonaUniTn@users.noreply.github.com>
2021-12-21 10:58:32 +0100

    Dashboard
    Portata a light

---
+++ dashboard.js
@@ -0,0 +1,62 @@
+function addDashboard(){
+    let layoutPrincipale =  '<div class="container rounded-pill" style="background-color: #272727; max-width: 300px;">' +
+                                '<p class="h6" style="color: beige;">' + Mese + '&emsp;|&emsp; ' + Proprietà + '</p>' +
+                            '</div>' +
+                            '<div class="bg-color " style="background-color: #272727; border-radius: 1ch; height: 150px; margin-top: 15px; margin-left: 0px; margin-right: 0px;">' +
+                                
+                                '<h4 class="display-6" style="color: beige; text-align: left; margin-left: 20px; margin-top: 10px;">' +
+                                    'Consumo</br>' +
+                                    'mensile' +
+                                    '<p class="h1" style="color: beige; text-align: right; text-align: end; margin-right: 60px; font-size: 50px; margin-bottom: 40px;">' + 112 + 'kW/H</p>' +
+                                '</h4>' +
+                            '</div>' +
+                            '</div>' +
+                            '<div class="container">' +
+                                '<div class="row ">' +
+                                    '<div class="col-md-6" style="margin-right: 0%;">' +
+                                        '<div class="card text-center" style="background-color: #272727; border: #272727; margin-top: 15px;">' +
+                                            '<div class="card-body " style="background-color: #272727; border-color: #272727; border-radius: 3ch;">' +
+                                                '<h2 class="card-title" style="color: beige;">' + 
+                                                    '9' +
+                                                '</h2>' +
+                                                '<p class="card-text" style="color: beige;">Camera</p>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                        
+                                '<div class="bg-color " style="background-color: #272727; border-radius: 3px; height: 150px; margin-top: 15px; margin-left: 0px; margin-right: 0px;">' +
+                                    
+                                    '<h4 class="display-6" style="color: beige; text-align: left; margin-left: 20px; margin-top: 10px;">' +
+                                        'Media</br>' +
+                                        'mensile' +
+                                        '<p class="h1" style="color: beige; text-align: right; text-align: end; margin-right: 60px; font-size: 50px; margin-bottom: 40px;">125 kW/H</p>' +
+                                    '</h4>' +
+                                '</div>' +
+                            '</br>';
+}
+
+
+function addConsumiStanze(){
+    let numeroStanze = stanze.lenght;
+
+
+}
+
+
+
+
+
+
+
+
+/*
+lista domande:
+-light o dark
+-consumi come gestiamo
+-funzioni add e modifica che famo
+
+
+-read me
+
+*/
