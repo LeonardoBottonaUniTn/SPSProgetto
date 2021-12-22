0f9dbbc02c1fdce6841d6d990b6ad0ff530ed2f4
LeonardoBottonaUniTn <95630334+LeonardoBottonaUniTn@users.noreply.github.com>
2021-12-22 21:30:04 +0100

    Revert "Merge branch 'api'"
    This reverts commit cc142108d8eeae67f12c49d51810cac373f091f5, reversing
    changes made to cb103e6ecea0e98dfd4336a9ac5e4fbaf3f87e62.

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
 var Express = require ("express");
 var bodyParser = require ("body-parser");
 var fs = require('fs');
-'use strict';
 
-var server = require('./server/app');
-var port = process.env.PORT || 49146;
+// add MongoDB Client
+var MongoClient = require ("mongodb").MongoClient;
 
-server.listen(port, function () {
-    console.log('Server running on port %d', port);
+// definition of the connection string
+var CONNECTION_STRING = "mongodb+srv://G38:pass_G38@sps.94fac.mongodb.net/test";
+
+var app = Express ();
+app.use(bodyParser.json ());
+app.use(bodyParser.urlencoded ({ extended: true }) );
+
+//name of the created MongoDB
+var DATABASE = "testDB";
+var database;
+
+const swaggerJsDoc = require ('swagger-jsdoc');
+const swaggerUI = require ('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Customer API",
+            description: "Customer API Information",
+            contact: {
+                name: "Leonardo Bottona"
+            },
+            servers: ["http://localhost/49146"]
+        }
+    },
+    apis: ["index.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use ('/api-docs', swaggerUI.serve, swaggerUI.setup (swaggerDocs));
+
+app.listen(49146, () => { 
+    //MongoDB connection
+    MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true,
+    useUnifiedTopology: true}, (error, client) => {
+        if(error){
+            console.log("Error connecting at the MongoDB: "+ error);
+        }else{
+            database = client.db(DATABASE);
+            console.log("MongoDB Connection Successfull");
+        }
+    })
 });
 
 var cors = require('cors');
 app.use(cors());
 
+app.get( '/', (request, response) => {
+    response.send('Ciao Gaia');
+})
 
 app.get ('/proprieta', (requenst, response) => {
     const userRouter = require ('./proprieta');
